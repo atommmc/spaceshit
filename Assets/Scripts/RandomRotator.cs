3230ea1963f5a394d6f3bc12f8112788dd1627b3
Alexander Stadnyk <Alexander Stadnyk>
2016-10-08 18:18:07 +0300

    Spawning waves -12- Space Shooter

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tuble;
+
+    void Start()
+    {
+        var astertics = GetComponent<Rigidbody>();
+        astertics.angularVelocity = Random.insideUnitSphere * tuble;
+    }
+}
